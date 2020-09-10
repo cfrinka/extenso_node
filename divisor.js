56b9a9574da0bf3153b92c05bcb7ff8e2797499b
Carlos Frinka Neto <c.frinkaneto@gmail.com>
2020-09-10 18:28:50 -0300

    deleted unused files

--- divisor.js
+++ divisor.js
@@ -6,34 +6,38 @@ const {
         hundreds,
 
     }
-} = require('./enumhelper')
+} = require('./enumhelper');
+const rs = require ('readline-sync')
 
+const userInput = rs.questionInt('Digite um valor: ')
 
+function extenso(input) {
 
-const divisor = 100
-const input = 211
-const centenas = parseInt(input/divisor)
-const resto = input%divisor
-const dezenas = parseInt(resto/10)
-const exceção = ''
-const unidades = resto%10
-let resultado =''
-
- if (dezenas == 1) {
+    const divisor = 100
+    const centenas = parseInt(input/divisor)
+    const resto = input%divisor
+    const dezenas = parseInt(resto/10)
+    const unidades = resto%10
+    let resultado =''
     
+    if (dezenas == 1) {
+        
+        
+        resultado = `${hundreds[centenas]} e ${tenToNineteen[unidades]}`
+        console.log(resultado)
 
-    resultado = `${hundreds[centenas]} e ${tenToNineteen[unidades]}`
-
-} else {
-
-    resultado = `${hundreds[centenas]} e ${tens[dezenas]} e ${uptoNine[unidades]}`
+    } else {
+        
+        resultado = `${hundreds[centenas]} e ${tens[dezenas]} e ${uptoNine[unidades]}`
+        console.log(resultado)
+    } 
     
-} 
+}
+
+extenso(userInput)
 
 
 
 
 
 
-
-console.log(resultado)
