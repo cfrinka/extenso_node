607a96683736d892b08da99878a2ae4c0636ff73
Carlos Frinka Neto <c.frinkaneto@gmail.com>
2020-09-11 10:09:26 -0300

    reais and centavos working

--- divisor.js
+++ divisor.js
@@ -7,11 +7,13 @@ const {
 
     }
 } = require('./enumhelper');
-const rs = require ('readline-sync')
+const rs = require('readline-sync')
 
-const userInput = rs.questionInt('Digite um valor: ')
 
-function extenso(input) {
+const userInputReais = 512
+const userInputCentavos = 12
+
+function extensoReais(input) {
 
     const divisor = 100
     const centenas = parseInt(input/divisor)
@@ -23,18 +25,44 @@ function extenso(input) {
     if (dezenas == 1) {
         
         
-        resultado = `${hundreds[centenas]} e ${tenToNineteen[unidades]}`
+        resultado = `${hundreds[centenas]} e ${tenToNineteen[unidades]} reais`
         console.log(resultado)
 
     } else {
         
-        resultado = `${hundreds[centenas]} e ${tens[dezenas]} e ${uptoNine[unidades]}`
+        resultado = `${hundreds[centenas]} e ${tens[dezenas]} e ${uptoNine[unidades]} reais`
         console.log(resultado)
     } 
     
 }
 
-extenso(userInput)
+function extensoCentavos(input) {
+    const divisor = 10
+    const dezenas = parseInt(input/10)
+    const unidades = userInputCentavos%10
+    let resultado =''
+    
+    if (dezenas == 1) {
+        
+        
+        resultado = `e ${tenToNineteen[unidades]} centavos`
+        console.log(resultado)
+
+    } if(dezenas > 1) {
+        
+        resultado = `e ${tens[dezenas]} e ${uptoNine[unidades]} centavos`
+        console.log(resultado)
+    } else {
+        resultado = `e ${uptoNine[unidades]}`
+    }
+    
+}
+
+
+
+
+extensoReais(userInputReais)
+extensoCentavos(userInputCentavos)
 
 
 
--- enumhelper.js
+++ enumhelper.js
@@ -2,7 +2,7 @@
 
 
 const numbers = Object.freeze({
-    uptoNine : ['','um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove',],
+    uptoNine : ['zero','um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove',],
     tenToNineteen: ['dez', 'onze','doze', 'treze', 'quatorze', 'quinze','dezesseis', 'dezessete', 'dezoito', 'dezenove'],
     tens:  ['','', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'oitenta','noventa'],
     hundreds: ['cem', 'cento', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos' ,'oitocentos', 'novecentos'],
